8440f6db9823ac9b26456069867ec5758dcc09d2
Mattia <mattia.ferraris@student.supsi.ch>
2026-06-12 16:08:33 +0100

    deleted not used classes in backend

--- backend/src/devices/app.module.ts
+++ backend/src/devices/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 import { TapoModule } from './tapo/tapo.module';
 import { KasaModule } from './kasa/kasa.module';
@@ -8,7 +6,7 @@ import { TplinkCloudModule } from './tplink-cloud/tplink-cloud.module';
 
 @Module({
   imports: [TapoModule, KasaModule, TplinkCloudModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
